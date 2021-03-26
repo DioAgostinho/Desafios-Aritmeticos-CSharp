dad9489eab871124654bc7236724a59b8bbded1f
Agostinho <agostinhonlpb@gmail.com>
2021-03-25 22:08:41 -0300

    1º commit

---
+++ 5.5 - Comunicação em Piralandia/Program.cs
@@ -0,0 +1,13 @@
+﻿using System; 
+
+class URI {
+    static void Main(string[] args) 
+    { 
+        string n = Console.ReadLine();
+        char[] arr = n.ToCharArray();
+        Array.Reverse(arr);
+           
+        string v = new string(arr);
+        Console.WriteLine(v);
+    }
+}
